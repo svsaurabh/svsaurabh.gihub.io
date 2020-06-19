4c61e961919cd0ae1d240043a07a98b167f6de2f
svsaurabh <svsaurabh3232@gmail.com>
2020-06-20 03:26:40 +0530

    Static react test

---
+++ precache-manifest.f692e22c79b21ad5e046ebbb09a6ae02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb926814c71a8e7994ebf79fb93ffa0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "188e0018c2830c837177",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f715bb96e5afb848d0c",
+    "url": "/static/js/2.528a25bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.528a25bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188e0018c2830c837177",
+    "url": "/static/js/main.880dfd76.chunk.js"
+  },
+  {
+    "revision": "8299da951d6793dc302d",
+    "url": "/static/js/runtime-main.2dee6ead.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
